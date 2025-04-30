99870f479d9a1c3284918aba8c958f2a8b1451df
Kakrote <anshul.pundir111@gmail.com1>
2025-05-01 04:58:24 +0530

    secound

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+
+  useEffect(()=>{
+    const token=localStorage.getItem("token")
+    if(token){
+      router.push('/dashbord')
+    }
+  })
 
   const handleStart = () => {
     router.push("/login");
